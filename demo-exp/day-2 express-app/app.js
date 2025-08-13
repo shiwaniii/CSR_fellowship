7aa192c612016e46b94bafd5ca7c9617c8e6bf2d
shiwaniii <Shiwani.adhikari@gmail.com>
2025-08-13 14:35:42 +0545

    added location, name and country

--- demo-exp/day-2 express-app/app.js
+++ demo-exp/day-2 express-app/app.js
@@ -6,6 +6,26 @@ app.get("/users", (req,res) => {
 
 });
 
+app.get("/Shiwani", (req,res) => {
+    res.send("Hello, Shiwani");
+
+});
+
+app.get("/users/:id", (req,res) => {
+    const userId = req.params.id;
+    res.send(`Hello, ${userId}`);
+
+});
+
+app.get("/home", (req,res) => {
+    const location = req.query.location;
+    const name = req.query.name;
+    const country = req.query.country;
+    res.send(`This is static route; location:${location}, name:${name}, country:${country}`);
+
+
+});
+
 app.listen(3000, () => {
     console.log("Express app is running at port 3000");
 });
